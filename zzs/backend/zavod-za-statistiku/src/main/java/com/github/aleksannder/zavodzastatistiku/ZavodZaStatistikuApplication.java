4faa0ab993f366c5e831a74e35da374217a87b47
Aleksander Zajac <aleksannder.z@gmail.com>
2025-09-08 19:50:01 +0200

    [develop]:: scaffold spring-boot project

---
+++ zzs/backend/zavod-za-statistiku/src/main/java/com/github/aleksannder/zavodzastatistiku/ZavodZaStatistikuApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleksannder.zavodzastatistiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavodZaStatistikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZavodZaStatistikuApplication.class, args);
+	}
+
+}
---
+++ zzs/backend/zavod-za-statistiku/src/test/java/com/github/aleksannder/zavodzastatistiku/ZavodZaStatistikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aleksannder.zavodzastatistiku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavodZaStatistikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
